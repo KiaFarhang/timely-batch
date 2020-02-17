771ea69553e238a6aa0f6488f50533f1b355f81f
Kia Farhang <kfarhang0@gmail.com>
2020-02-16 18:33:19 -0600

    chore: Spring Initializr setup

---
+++ src/main/java/com/kiafarhang/timely/batch/TimelyBatchApplication.java
@@ -0,0 +1,13 @@
+package com.kiafarhang.timely.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelyBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelyBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiafarhang/timely/batch/TimelyBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiafarhang.timely.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelyBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
