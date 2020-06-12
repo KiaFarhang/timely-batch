fd60befb1d230151cd115c4779af9aa84d2f1eae
Kia Farhang <kfarhang0@gmail.com>
2020-06-11 19:29:18 -0500

    feature: add batch job skeleton

--- src/main/java/com/kiafarhang/timely/batch/TimelyBatchApplication.java
+++ src/main/java/com/kiafarhang/timely/batch/TimelyBatchApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TimelyBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TimelyBatchApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TimelyBatchApplication.class, args);
+  }
 }
